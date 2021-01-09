f8c5186ec038c3ad1a6c5c57e9e1974920730582
Mubeen Rao <74795094+Mubeenrao015@users.noreply.github.com>
2021-01-09 09:47:46 +0500

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+// Pages import
+import 'package:todo_tasks/pages/LoginPage.dart';
+import 'package:todo_tasks/pages/TodosPage.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(new MaterialApp(
+    home: AppSplash(),
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      primarySwatch: Colors.deepOrange,
+      visualDensity: VisualDensity.adaptivePlatformDensity,
+    ),
+  ));
+}
+
+class AppSplash extends StatefulWidget {
+  @override
+  _AppSplashState createState() => new _AppSplashState();
+}
+
+class _AppSplashState extends State<AppSplash> {
+  @override
+  Widget build(BuildContext context) {
+    return new SplashScreen(
+      seconds: 5,
+      navigateAfterSeconds: new MyHomePage(),
+      title: new Text(
+        'Todos App',
+        style: new TextStyle(
+            fontWeight: FontWeight.bold, fontSize: 30.0, color: Colors.white),
+      ),
+      image: new Image.asset('assets/images/farman.jpg'),
+      backgroundColor: Colors.deepOrange,
+      loaderColor: Colors.white,
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  checkAuthentication() async {
+    _auth.onAuthStateChanged.listen((user) {
+      if (user != null) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => TodosPage(),
+          ),
+        );
+      } else {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => LoginPage(),
+          ),
+        );
+      }
+    });
+  }
+
+  void initState() {
+    super.initState();
+    this.checkAuthentication();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
