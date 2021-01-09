02b426031a26367bd252af44a58efa801ca1dbe5
Mubeen Rao <74795094+Mubeenrao015@users.noreply.github.com>
2021-01-09 12:49:25 +0500

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,16 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_login_ui/screens/login_screen.dart';
+import 'package:injectable/injectable.dart';
+import 'package:notes_firebase_ddd_course/injection.dart';
+import 'package:notes_firebase_ddd_course/presentation/core/app_widget.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Login UI',
-      debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
-    );
-  }
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  configureInjection(Environment.prod);
+  await Firebase.initializeApp();
+  runApp(AppWidget());
 }
